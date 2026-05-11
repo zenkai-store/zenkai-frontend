500e3ab5980db7d3357ec91e14c54e866927705d
nkl0zip <nikhilkumarpatra36@gmail.com>
2026-05-11 21:53:01 +0530

    updated prod origin link

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -3,6 +3,6 @@
 const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://zenkai-backend.onrender.com/";
+    : "https://zenkai-backend.onrender.com";
 
 export default BASE_URL;
